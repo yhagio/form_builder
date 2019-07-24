f14ae98e5ce7bb4cb324187ff7b1d80b54641455
Yuichi Hagio <yhagio87@gmail.com>
2019-07-23 20:51:05 -0400

    golden file

--- html_test.go
+++ html_test.go
@@ -1,8 +1,13 @@
 package form_builder_test
 
 import (
+	"flag"
+	"fmt"
 	"form_builder"
 	"html/template"
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -14,11 +19,17 @@ var (
 	)
 )
 
+var updateFlag bool
+
+func init() {
+	flag.BoolVar(&updateFlag, "update", false, "set the update flag to update the expected output of all golden file")
+}
+
 func TestHTML(t *testing.T) {
 	tests := map[string]struct {
 		tpl     *template.Template
 		strct   interface{}
-		want    template.HTML
+		want    string
 		wantErr error
 	}{
 		"Simple form with values": {
@@ -30,8 +41,7 @@ func TestHTML(t *testing.T) {
 				Name:  "Alice Smith",
 				Email: "alice@cc.cc",
 			},
-			want: `<input type="text" name="Name" value="Alice Smith">` +
-				`<input type="text" name="Email" value="alice@cc.cc">`,
+			want: "TestHTML_basic.golden",
 		},
 	}
 
@@ -41,9 +51,45 @@ func TestHTML(t *testing.T) {
 			if err != tc.wantErr {
 				t.Fatalf("HTML() err %v; wantErr %v", err, tc.wantErr)
 			}
-			if got != tc.want {
-				t.Errorf("HTML() got %v; want %v", got, tc.want)
+
+			gotFilename := strings.Replace(tc.want, ".golden", ".got", 1)
+			os.Remove(gotFilename)
+
+			if updateFlag {
+				writeFile(t, tc.want, string(got))
+				t.Logf("Updated golden file %s", tc.want)
+			}
+
+			want := template.HTML(readFile(t, tc.want))
+			if got != want {
+				t.Errorf("HTML() - results do not match golden file.")
+				writeFile(t, gotFilename, string(got))
+				t.Errorf(" To compare run: diff %s %s", gotFilename, tc.want)
 			}
 		})
 	}
+}
+
+func writeFile(t *testing.T, filename, contents string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		t.Fatalf("Error creating file %v: %v", file, err)
+	}
+	defer file.Close()
+	fmt.Fprint(file, contents)
+}
+
+func readFile(t *testing.T, filename string) []byte {
+	file, err := os.Open(filename)
+	if err != nil {
+		t.Fatalf("Error opening file %v: %v", file, err)
+	}
+	defer file.Close()
+
+	byte, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Fatalf("Error reading file %v: %v", file, err)
+	}
+
+	return byte
 }
